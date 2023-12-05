c184a3639e362b14b077a69b5a063150032e90ab
Nikitapoddar01 <145185794+Nikitapoddar01@users.noreply.github.com>
2023-12-05 11:55:09 +0545

    Add files via upload

---
+++ html12.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Create a p element with some text:</p>
+
+<script>
+constpara = document.createElement("p");
+para.innerText="This is a paragraph.";
+
+document.body.appendChild(para);
+</script>
+    
+</body>
+</html>
+
